8cfdcf43a17963528f8c4d7278ae7fd3ba578cec
AaronLeong <626786864@qq.com>
2018-07-19 15:38:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 
 const mssql = require('./mssql.js');  
-let defaultconfig = {
+const defaultconfig = {
     userName: 'admin',
     password: 'c0mm0n-adm1n-new',
     server:'',
     options: {database: 'test'}
 };
-var conn = new mssql.mssql(defaultconfig);   
+const conn = new mssql.mssql(defaultconfig);  
+
 function mssql_query(sql) {
-  let data
-  data = new Promise(function (resolve, reject) {
+  return new Promise((resolve, reject) => {
     conn.query(sql,function(err,data){
       if (err) {
         reject(err)
@@ -21,6 +21,4 @@ function mssql_query(sql) {
 
 mssql_query("select * from t_project_sms").then(data => console.log(data)).catch(err => console.log(err))
 
-// let data = await mssql_query("select * from t_project_sms"); 
-// console.log(data)
 
